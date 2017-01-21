fdffa81866ead176e8c5260c8195a673070370d2
Mark Elrod <git-mark@helixport.com>
2017-01-20 23:10:27 -0600

    Added a script to automatically run make check and then email the results, timer.sh.

---
+++ scripts/timer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while true
+do
+while [ $(date +%H:%M) != "$1" ]; do
+date +%H:%M
+    sleep 1
+done
+echo "Testing build"
+make check
+echo "Bravo test results are attached" | mutt -a *.log -s "Bravo Test Results" -- mark1@helixport.com
+
+sleep 61
+done
