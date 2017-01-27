8b89a1c777c8f7121d4ac3221151d1ec1aabb341
Mark Elrod <git-mark@helixport.com>
2017-01-26 19:23:12 -0600

    Fixed cmake builds for windows. Fixed FindCheck.cmake. Only looking for compat.lib if in windows. Both cmake and autotools now work for Linux.

--- cmake/Modules/FindCheck.cmake
+++ cmake/Modules/FindCheck.cmake
@@ -4,9 +4,21 @@ find_path(Check_INCLUDE_DIRS
   HINTS c:/check/include
 )
 
-find_library(Check_LIBRARIES
+find_library(Check_check
   NAMES check
   HINTS c:/check/lib
+)
+
+if (WIN32)
+find_library(Check_compat
+  NAMES compat
+  HINTS c:/check/lib
+)
+endif()
+
+set(Check_LIBRARIES
+${Check_check}
+${Check_compat}
 )
 
 
