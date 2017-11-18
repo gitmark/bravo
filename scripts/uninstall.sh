72caaa1981ccc91f3869ee1f76e43aa6df25fca7
Mark Elrod <git-mark@helixport.com>
2017-11-17 18:15:38 -0600

    updated uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 rm /usr/local/lib/libbravo.a
+rm /usr/local/lib/libbravo1.a
+rm /usr/local/lib/libbravo.dylib
+rm /usr/local/lib/libbravo1.dylib
 rm /usr/local/include/bravo
 rm /usr/local/bin/baretta
 rm /usr/local/bin/serrano
-sudo rm -rf /usr/local/bravo
+sudo rm -rf /usr/local/bravo/brv1
+sudo rm -rf /usr/local/bravo/brv1.0
+sudo rm -rf /usr/local/bravo/brv1.0.0
+
